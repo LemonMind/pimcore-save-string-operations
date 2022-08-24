0c1c8339f28ca01f3c3dc7678c4ceb17f75957a5
Pawel Filipkowski <p.filipkowski@lemonmind.com>
2022-08-24 09:41:03 +0200

    add testGetParams

--- src/SaveStringReplaceBundle/Tests/Controller/StringConcatControllerTest.php
+++ src/SaveStringReplaceBundle/Tests/Controller/StringConcatControllerTest.php
@@ -8,6 +8,7 @@ use Lemonmind\SaveStringReplaceBundle\Controller\StringConcatController;
 use Lemonmind\SaveStringReplaceBundle\Tests\TestObject\TestObject;
 use Pimcore\Test\KernelTestCase;
 use ReflectionClass;
+use Symfony\Component\HttpFoundation\Request;
 
 class StringConcatControllerTest extends KernelTestCase
 {
@@ -19,7 +20,16 @@ class StringConcatControllerTest extends KernelTestCase
      *
      * @throws \ReflectionException
      */
-    public function testStringConcat(string $name, string $description, array $fields, string $userInput, string $separator, string $fieldToSaveConcat, string $expected, int $productNumber): void
+    public function testStringConcat(
+        string $name,
+        string $description,
+        array  $fields,
+        string $userInput,
+        string $separator,
+        string $fieldToSaveConcat,
+        string $expected,
+        int    $productNumber
+    ): void
     {
         for ($i = 0; $i < $productNumber; ++$i) {
             $this->objectListing[] = new TestObject($name, $description);
@@ -55,6 +65,65 @@ class StringConcatControllerTest extends KernelTestCase
             ['lorem', 'some text', ['input', 'description'], 'input text', ' ', 'description', 'input text some text', 10],
             ['lorem', 'some text', ['name', 'input'], 'input text', ' ', 'name', 'lorem input text', 1],
             ['lorem', 'some text', ['name', 'input'], 'input text', ' ', 'name', 'lorem input text', 10],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider dataProviderParams
+     *
+     * @throws \ReflectionException
+     */
+    public function testGetParams(
+        string $fieldOne,
+        string $fieldTwo,
+        array  $expectedFields,
+        string $fieldToSaveConcat,
+        string $input,
+        string $separator,
+        string $idList,
+        array  $expectedIds,
+        string $className,
+        string $expectedClassName
+    ): void
+    {
+        $request = $this->createStub(Request::class);
+
+        if ('input' === $fieldOne) {
+            $request->method('get')
+                ->withConsecutive(['field_one'], ['field_two'], ['field_save'], ['input_one'], ['separator'], ['idList'], ['className'])
+                ->willReturnOnConsecutiveCalls($fieldOne, $fieldTwo, $fieldToSaveConcat, $input, $separator, $idList, $className);
+        } elseif ('input' === $fieldTwo) {
+            $request->method('get')
+                ->withConsecutive(['field_one'], ['field_two'], ['field_save'], ['input_two'], ['separator'], ['idList'], ['className'])
+                ->willReturnOnConsecutiveCalls($fieldOne, $fieldTwo, $fieldToSaveConcat, $input, $separator, $idList, $className);
+        } else {
+            $request->method('get')
+                ->withConsecutive(['field_one'], ['field_two'], ['field_save'], ['separator'], ['idList'], ['className'])
+                ->willReturnOnConsecutiveCalls($fieldOne, $fieldTwo, $fieldToSaveConcat, $separator, $idList, $className);
+        }
+
+        $controller = new StringConcatController();
+        $reflector = new ReflectionClass($controller);
+        $method = $reflector->getMethod('getParams');
+
+        $method->invokeArgs($controller, [$request, true]);
+        $this->assertSame($expectedFields, $reflector->getProperty('fields')->getValue($controller));
+        $this->assertSame($fieldToSaveConcat, $reflector->getProperty('fieldToSaveConcat')->getValue($controller));
+        $this->assertSame($expectedClassName, $reflector->getProperty('class')->getValue($controller));
+        $this->assertSame($expectedIds, $reflector->getProperty('ids')->getValue($controller));
+
+        if ('' !== $input) {
+            $this->assertSame($input, $reflector->getProperty('userInput')->getValue($controller));
+        }
+    }
+
+    public function dataProviderParams(): array
+    {
+        return [
+            ['name', 'description', ['name', 'description'], 'name', '', ' ', '', [], 'class', '\Pimcore\Model\DataObject\class\Listing'],
+            ['input', 'description', ['input', 'description'], 'description', 'some text', ',', '1,2,3', ['1', '2', '3'], ' ', '\Pimcore\Model\DataObject\ \Listing'],
+            ['name', 'input', ['name', 'input'], 'name', 'some text', ', ', '1,2,3,4,5', ['1', '2', '3', '4', '5'], 'class', '\Pimcore\Model\DataObject\class\Listing'],
         ];
     }
 }
