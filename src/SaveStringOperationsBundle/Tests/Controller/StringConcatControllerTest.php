b17aef589f2a2ecae288c31045b7575234220180
Pawel Filipkowski <p.filipkowski@lemonmind.com>
2022-08-25 15:27:03 +0200

    remove tests for string methods

--- src/SaveStringOperationsBundle/Tests/Controller/StringConcatControllerTest.php
+++ src/SaveStringOperationsBundle/Tests/Controller/StringConcatControllerTest.php
@@ -5,69 +5,12 @@ declare(strict_types=1);
 namespace Lemonmind\SaveStringOperationsBundle\Tests\Controller;
 
 use Lemonmind\SaveStringOperationsBundle\Controller\StringConcatController;
-use Lemonmind\SaveStringOperationsBundle\Tests\TestObject\TestObject;
 use Pimcore\Test\KernelTestCase;
 use ReflectionClass;
 use Symfony\Component\HttpFoundation\Request;
 
 class StringConcatControllerTest extends KernelTestCase
 {
-    private array $objectListing;
-
-    /**
-     * @test
-     * @dataProvider dataProvider
-     *
-     * @throws \ReflectionException
-     */
-    public function testStringConcat(
-        string $name,
-        string $description,
-        array $fields,
-        string $userInput,
-        string $separator,
-        string $fieldToSaveConcat,
-        string $expected,
-        int $productNumber
-    ): void {
-        for ($i = 0; $i < $productNumber; ++$i) {
-            $this->objectListing[] = new TestObject($name, $description);
-        }
-        $controller = new StringConcatController();
-        $reflector = new ReflectionClass($controller);
-
-        $reflector->getProperty('fields')->setValue($controller, $fields);
-        $reflector->getProperty('userInput')->setValue($controller, $userInput);
-        $reflector->getProperty('fieldToSaveConcat')->setValue($controller, $fieldToSaveConcat);
-        $reflector->getProperty('separator')->setValue($controller, $separator);
-
-        $method = $reflector->getMethod('stringConcat');
-        $method->invokeArgs($controller, [$this->objectListing]);
-
-        for ($i = 0; $i < $productNumber; ++$i) {
-            /* @phpstan-ignore-next-line */
-            $this->assertEquals($expected, $this->objectListing[$i]->get($fieldToSaveConcat));
-        }
-    }
-
-    public function dataProvider(): array
-    {
-        return [
-            ['lorem', 'some text', ['name', 'description'], '', ' ', 'name', 'lorem some text', 1],
-            ['lorem', 'some text', ['name', 'description'], '', ' ', 'name', 'lorem some text', 10],
-            ['lorem', 'some text', ['name', 'description'], '', ' ', 'description', 'lorem some text', 1],
-            ['lorem', 'some text', ['name', 'description'], '', ' ', 'description', 'lorem some text', 10],
-            ['lorem', 'some text', ['description', 'name'], '', ' ', 'name', 'some text lorem', 1],
-            ['lorem', 'some text', ['description', 'name'], '', ' ', 'name', 'some text lorem', 10],
-            ['lorem', 'some text', ['name', 'description'], '', ',', 'name', 'lorem,some text', 1],
-            ['lorem', 'some text', ['name', 'description'], '', ',', 'name', 'lorem,some text', 10],
-            ['lorem', 'some text', ['input', 'description'], 'input text', ' ', 'description', 'input text some text', 1],
-            ['lorem', 'some text', ['input', 'description'], 'input text', ' ', 'description', 'input text some text', 10],
-            ['lorem', 'some text', ['name', 'input'], 'input text', ' ', 'name', 'lorem input text', 1],
-            ['lorem', 'some text', ['name', 'input'], 'input text', ' ', 'name', 'lorem input text', 10],
-        ];
-    }
-
     /**
      * @test
      * @dataProvider dataProviderParams
--- src/SaveStringOperationsBundle/Tests/Controller/StringReplaceControllerTest.php
+++ src/SaveStringOperationsBundle/Tests/Controller/StringReplaceControllerTest.php
@@ -5,58 +5,12 @@ declare(strict_types=1);
 namespace Lemonmind\SaveStringOperationsBundle\Tests\Controller;
 
 use Lemonmind\SaveStringOperationsBundle\Controller\StringReplaceController;
-use Lemonmind\SaveStringOperationsBundle\Tests\TestObject\TestObject;
 use Pimcore\Test\KernelTestCase;
 use ReflectionClass;
 use Symfony\Component\HttpFoundation\Request;
 
 class StringReplaceControllerTest extends KernelTestCase
 {
-    private array $objectListing;
-
-    /**
-     * @test
-     * @dataProvider dataProvider
-     *
-     * @throws \ReflectionException
-     */
-    public function testStringReplace(string $name, string $search, string $replace, string $expected, bool $isInsensitive, int $productNumber): void
-    {
-        for ($i = 0; $i < $productNumber; ++$i) {
-            $this->objectListing[] = new TestObject($name, '');
-        }
-        $this->objectListing[] = new TestObject('different name', '');
-        $controller = new StringReplaceController();
-        $reflector = new ReflectionClass($controller);
-
-        $reflector->getProperty('field')->setValue($controller, 'name');
-        $reflector->getProperty('search')->setValue($controller, $search);
-        $reflector->getProperty('replace')->setValue($controller, $replace);
-        $reflector->getProperty('isInsensitive')->setValue($controller, $isInsensitive);
-
-        $method = $reflector->getMethod('stringReplace');
-        $method->invokeArgs($controller, [$this->objectListing]);
-
-        for ($i = 0; $i < $productNumber - 1; ++$i) {
-            /* @phpstan-ignore-next-line */
-            $this->assertEquals($expected, $this->objectListing[$i]->get('name'));
-        }
-        /* @phpstan-ignore-next-line */
-        $this->assertEquals('different name', $this->objectListing[$productNumber]->get('name'));
-    }
-
-    public function dataProvider(): array
-    {
-        return [
-            ['lorem', 'lorem', 'ipsum', 'ipsum', false, 1],
-            ['lorem', 'lorem', 'ipsum', 'ipsum', false, 5],
-            ['lorem', 'lorem', 'ipsum', 'ipsum', false, 40],
-            ['lorem ipsum', 'lorem', 'ipsum', 'ipsum ipsum', false, 10],
-            ['loREm ipsum', 'lorem', 'ipsum', 'ipsum ipsum', true, 10],
-            ['loREm ipsum', 'LOREm', 'ipsum', 'ipsum ipsum', true, 10],
-        ];
-    }
-
     /**
      * @test
      * @dataProvider dataProviderParams
@@ -86,7 +40,7 @@ class StringReplaceControllerTest extends KernelTestCase
         $method = $reflector->getMethod('getParams');
         $method->invokeArgs($controller, [$request, true]);
         /* @phpstan-ignore-next-line */
-        $this->assertSame($field, $reflector->getProperty('field')->getValue($controller));
+        $this->assertSame([$field], $reflector->getProperty('field')->getValue($controller));
         /* @phpstan-ignore-next-line */
         $this->assertSame($search, $reflector->getProperty('search')->getValue($controller));
         /* @phpstan-ignore-next-line */
